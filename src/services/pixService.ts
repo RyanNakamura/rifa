f8752daf533ab6fdbe14f138e7f22fd85c755dd3
RyanNakamura <164840637+RyanNakamura@users.noreply.github.com>
2025-07-26 12:50:18 -0300

    Updated pixService.ts

--- src/services/pixService.ts
+++ src/services/pixService.ts
@@ -2,8 +2,8 @@ import { PixResponse } from '../types';
 
 const SECRET_KEY = 'c6b41266-2357-4a6c-8e07-aa3873690c1a';
 const PUBLIC_KEY = '4307a311-e352-47cd-9d24-a3c05e90db0d';
-const API_URL = 'https://app.ghostspaysv1.com/api/v1/transaction.purchase';
-const STATUS_CHECK_URL = 'https://app.ghostspaysv1.com/api/v1/transaction.getPayment';
+const API_URL = 'https://app.ghostspaysv1.com/api/v1/transaction/purchase';
+const STATUS_CHECK_URL = 'https://app.ghostspaysv1.com/api/v1/transaction/getPayment';
 
 export async function gerarPix(
   name: string,
